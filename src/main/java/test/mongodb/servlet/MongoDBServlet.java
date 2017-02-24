98be4f3ada825440f17746bb14724e63fe39f5e8
manisha3 <manisha.pundir@atos.net>
2017-02-24 14:22:47 +0530

    Update MongoDBServlet.java

--- src/main/java/test/mongodb/servlet/MongoDBServlet.java
+++ src/main/java/test/mongodb/servlet/MongoDBServlet.java
@@ -54,11 +54,11 @@ public class MongoDBServlet extends HttpServlet {
         String db = "sampledb";
         if(db == null)
             db = "mydb";
-       /* String user = "admin";
-        String password = "c7itwQMbwfcVsKN2"; */
+        String user = "admin";
+        String password = "c7itwQMbwfcVsKN2"; 
         int port = Integer.decode(sport);
-
-/*        try 
+	    
+     try 
 	{
             mongo = new Mongo("172.30.143.34", port);
         } 
@@ -69,7 +69,7 @@ public class MongoDBServlet extends HttpServlet {
            mongoDB = mongo.getDB(db);
         if(mongoDB.authenticate(user, password.toCharArray()) == false) {
             throw new ServletException("Failed to authenticate against db: "+db);
-        } */
+        } 
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
